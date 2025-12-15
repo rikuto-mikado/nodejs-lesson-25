e36f8f091813ced8d95564a4cd104e277d9be821
Rikuto <mikadorikuto@gmail.com>
2025-12-15 15:18:45 +0900

    update the project

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const express = require('express');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+    res.render();
+});
+
+app.get('users', (req, res, next) => {
+    res.render();
+});
+
+app.post('add-user', (req, res, next) => {
+    res.redirect('/users');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running`);
+})
