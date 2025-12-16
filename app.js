f94b2a8ed667718088b8dfee8f49671cd3092545
Rikuto <mikadorikuto@gmail.com>
2025-12-16 23:04:13 +0900

    update the code

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+const users = [];
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res, next) => {
-    res.render();
+    res.render('index', { pageTitle: 'Add User' });
 });
 
 app.get('users', (req, res, next) => {
-    res.render();
+    res.render('users', { pageTitle: 'User' });
 });
 
 app.post('add-user', (req, res, next) => {
+    users.push({ name: req.body.username });
     res.redirect('/users');
 });
 
